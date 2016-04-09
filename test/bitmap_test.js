64fd87938cfe29bc57ac455f9db5dbcbc96bdddb
BRANDON PARKER <Brandon@dkparker.com>
2016-04-09 10:19:26 -0700

    set up pending tests

--- test/bitmap_test.js
+++ test/bitmap_test.js
@@ -1,3 +1,23 @@
 const chai = require('chai');
 const expect = chai.expect;
-const bitmapCli = require(__dirname + '/bin/bitmap');
+// const bitmapCli = require(__dirname + '/bin/bitmap');
+
+
+//read bitmap
+//transform bitmap
+//write bitmap
+
+describe('Is new file created', () => {
+  it('filename_new.bmp should not exist', () => {
+
+  });
+
+  it('filename_new.bmp should exist', () => {
+
+  });
+
+  it('filename.bmp data should not match filename_new.bmp data', () => {
+
+  });
+
+});
